44eb64d687a1bdb208d4a1807dafd1fb5e40af97
Fre251 <41941222+Fre251@users.noreply.github.com>
2018-07-31 13:11:57 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { Search1Pipe } from './search1.pipe';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    Search1Pipe,
+    DashboardComponent
+  
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/mock.heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id:1, name: "Iron Man",dept:"Marvel" },
+    {id:2, name: "Batman",dept:"DC" },
+    {id:3, name: "Captain America",dept:"Marvel" },
+    {id:4, name: "Wolverine",dept:"Xmen" },
+    {id:5, name: "Black Panther",dept:"Marvel" },
+    {id:6, name: "Dead Pool",dept:"Xmen" },
+    {id:7, name: "Aquaman",dept:"DC" },
+    {id:8, name: "Spiderman",dept:"Marvel" },
+    {id:9, name: "Flash",dept:"DC" },
+];
